78d527e361e78b62ddee7749833ce691b36db734
Lida Karapetian <108219180+LidaKarapetian@users.noreply.github.com>
2023-08-27 16:20:21 +0400

    Update Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -31,7 +31,19 @@ def Sorting(arr):
         smallest = SmallestIndex(arr)
         newArr.append(arr.pop(smallest))
     return newArr
-    
+
+#Implementation of Quick Sort
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
+
 
 
 
